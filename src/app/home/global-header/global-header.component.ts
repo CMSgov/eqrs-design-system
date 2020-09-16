fdff3cb47c51cb5165334696f3b0ce7889c67358
Sophia Dengo <sdengo@gmail.com>
2020-09-16 14:47:52 -0400

    changed header style file from .css to .scss

--- src/app/home/global-header/global-header.component.ts
+++ src/app/home/global-header/global-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-global-header',
   templateUrl: './global-header.component.html',
-  styleUrls: ['./global-header.component.css']
+  styleUrls: ['./global-header.component.scss']
 })
 export class GlobalHeaderComponent implements OnInit {
 

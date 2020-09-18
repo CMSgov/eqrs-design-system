a657b9da132ae9a79780084d41456ccfc5ddf367
Sophia Dengo <sdengo@gmail.com>
2020-09-18 11:36:28 -0400

    change footer css to scss

--- src/app/home/global-footer/global-footer.component.ts
+++ src/app/home/global-footer/global-footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-global-footer',
   templateUrl: './global-footer.component.html',
-  styleUrls: ['./global-footer.component.css']
+  styleUrls: ['./global-footer.component.scss']
 })
 export class GlobalFooterComponent implements OnInit {
 
